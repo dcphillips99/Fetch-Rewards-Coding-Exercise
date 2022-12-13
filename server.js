1cd99f3505dffa54ff7d6cf3b1ec89084d0ea986
dcphillips99 <dcphillips@wisc.edu>
2022-12-13 03:55:08 -0600

    Add trans route now sorts transactions, handles more than one call

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(express.json());
 
 var transactions;
+var totalPoints = 0;
 
 app.get('/', (req, res) => 
 {
@@ -31,12 +32,39 @@ app.post('/transaction-add', (req, res) =>
             res.send("Check that transactions are in the correct format: [{\"payer\": xxxx, \"points\": xxxx, \"timestamp\": \"2022-10-31T10:00:00Z\"}]");
             return;
         }
+        totalPoints += parseInt(transaction.points);
+        console.log(totalPoints);
     });
 
     transactions = req.body;
 
-    res.send("transaction recieved");
+    //this sorts the transactions array by timestamps in descending order (oldest first)
+    transactions.sort(function(x, y)
+    {
+        return new Date(x.timestamp) - new Date(y.timestamp);
+    });
+
     console.log(transactions);
+    res.send("transaction recieved");
+});
+
+//spend POST request should subtract the oldest points from the user's account.
+//First search based on oldest points (look for oldest year, month, then day)
+//Then use all of those points.
+app.post('/spend', (req, res) => 
+{
+    let spendAmount = req.body.points;
+
+    if (spendAmount > totalPoints)
+    {
+        res.send("The amount of points you want to spend exceeds the current balance.");
+        return;
+    }
+
+    for(var i = 0; i < transactions.length; ++i) 
+    {
+
+    }
 });
 
 app.listen(3000);
